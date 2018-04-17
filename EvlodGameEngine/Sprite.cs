02650eade625270ee7191d811a3c0b8bb0abce3a
movleax <6619780+movleax@users.noreply.github.com>
2018-04-16 21:19:12 -0700

    Added a test png. Added a Sprite class

---
+++ EvlodGameEngine/Sprite.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace EvlodGameEngine
+{
+    class Sprite
+    {
+        Texture2D texture;
+        Vector2 position;
+        Rectangle drawRect;
+        float alpha;
+        float rotation;
+        Vector2 origin;
+        float scale;
+        SpriteEffects spriteEffect;
+        float zDepth;
+
+        int rows;
+        int columns;
+        int clipWidth;
+        int clipHeight;
+        int currentFrame;
+        int totalFrames;
+
+
+        public Sprite(Texture2D Texture, int Rows, int Columns)
+        {
+            alpha = 1.0f;
+            rotation = 0.0f;
+            origin = new Vector2(0, 0);
+            scale = 1.0f;
+            spriteEffect = SpriteEffects.None;
+            zDepth = 0.1f;
+
+            texture = Texture;
+
+            rows = Rows;
+            columns = Columns;
+            currentFrame = 0;
+            totalFrames = columns * rows;
+
+            clipWidth = texture.Width / columns;
+            clipHeight = texture.Height / rows;
+            
+
+            drawRect = new Rectangle(0, 0, texture.Width, texture.Height);
+            position = new Vector2(0, 0);
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(
+                            texture,
+                            position,
+                            drawRect,
+                            Color.White * alpha,
+                            rotation,
+                            origin,
+                            scale,
+                            spriteEffect,
+                            zDepth
+                        );
+        }
+
+        public void IncrementFrame()
+        {
+            currentFrame = currentFrame < totalFrames ? currentFrame++ : 0;
+        }
+    }
+}
